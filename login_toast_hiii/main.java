3eb8d4de5551699214e5a63ec4bdaf4438e35fa7
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2022-03-31 11:04:02 +0530

    Create main.java

---
+++ login_toast_hiii/main.java
@@ -0,0 +1,22 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.bu1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "haiii", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
